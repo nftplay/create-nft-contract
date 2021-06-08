05e4ee6586eadeb26e1ad3ef0825b876b39f360f
NFT Play <82499092+nftplay@users.noreply.github.com>
2021-06-08 10:58:08 +0800

    upload contracts

---
+++ contracts/NFTPlay.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
+ 
+import "@openzeppelin/contracts/token/ERC721/ERC721Burnable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFTPlay is ERC721Burnable{
+	
+	   using Counters for Counters.Counter;
+	
+       Counters.Counter private _tokenIds;
+
+       constructor() ERC721("NFT Play", "NFTPLAY") {}
+   
+       function createNFT(address to, string memory tokenURI) public returns (uint256)
+       {
+           _tokenIds.increment();   
+           uint256 tokenId = _tokenIds.current();
+           _mint(to, tokenId);
+           _setTokenURI(tokenId, tokenURI);
+   
+           return tokenId;
+       }
+}
